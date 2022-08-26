6070328a97985511a9917de2dada5934608f0132
Kelder Passos <kelder.passos@gmail.com>
2022-08-26 15:26:52 -0300

    feat: começando req12

---
+++ src/services/post.js
@@ -0,0 +1,7 @@
+// const { Post } = require('../database/models');
+
+// module.exports = {
+//   // createNewPost: async (info) => {
+//   //   const newPost = await Post.create(info);
+//   // },
+// };
